3a58ef2da752a4a1d5d0205909ba690126b16530
Lukas Lang <ll542@cam.ac.uk>
2019-03-11 16:11:19 +0000

    Changed name of test folder to tests.

--- add-ons/TBIR/runtests.sh
+++ add-ons/TBIR/runtests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 \
 LD_LIBRARY_PATH=/home/ll542/store/git/astra/lib \
-nohup nice matlab-r2017b -nodisplay -nojvm -nosplash -nodesktop -r "run('../../FAIRstartup.m'); run('TBIRstartup.m'); try; runtests('test'); catch ME; disp(ME); disp(getReport(ME, 'extended', 'hyperlinks', 'off')); exit(1); end; exit(0);" > runtests.log 2>&1
+nohup nice matlab-r2017b -nodisplay -nojvm -nosplash -nodesktop -r "run('../../FAIRstartup.m'); run('TBIRstartup.m'); try; runtests('tests'); catch ME; disp(ME); disp(getReport(ME, 'extended', 'hyperlinks', 'off')); exit(1); end; exit(0);" > runtests.log 2>&1

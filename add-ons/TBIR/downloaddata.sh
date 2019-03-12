19173d553b3dad242fdee61887fac031c801b2cd
Lukas Lang <ll542@cam.ac.uk>
2019-03-12 17:37:13 +0000

    Added script to download the data.

---
+++ add-ons/TBIR/downloaddata.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd data
+curl https://zenodo.org/record/1254204/files/sinogram.mat?download=1
+curl https://zenodo.org/record/1254206/files/Data164.mat?download=1
+curl https://zenodo.org/record/1183532/files/DataDynamic_128x60.mat?download=1
+cd ..

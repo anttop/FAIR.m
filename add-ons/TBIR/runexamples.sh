adc304766fd9b174293902f40dc18cffefc73b2d
Lukas Lang <ll542@cam.ac.uk>
2019-03-12 17:42:51 +0000

    Added another example to run script.

--- add-ons/TBIR/runexamples.sh
+++ add-ons/TBIR/runexamples.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 \
 LD_LIBRARY_PATH=/home/ll542/store/git/astra/lib \
-nohup nice matlab-r2017b -nodisplay -nojvm -nosplash -nodesktop -r "run('../../FAIRstartup.m'); run('TBIRstartup.m'); try; run('ex_1a.m'); run('ex_1b.m'); run('ex_2.m'); run('ex_3.m'); run('ex_4.m'); run('ex_6.m'); run('ex_7.m'); run('ex_8.m'); run('ex_5.m'); catch ME; disp(ME); disp(getReport(ME, 'extended', 'hyperlinks', 'off')); exit(1); end; exit(0);" > runexamples.log 2>&1
+nohup nice matlab-r2017b -nodisplay -nojvm -nosplash -nodesktop -r "run('../../FAIRstartup.m'); run('TBIRstartup.m'); try; run('ex_0.m'); run('ex_1a.m'); run('ex_1b.m'); run('ex_2.m'); run('ex_3.m'); run('ex_4.m'); run('ex_6.m'); run('ex_7.m'); run('ex_8.m'); run('ex_5.m'); catch ME; disp(ME); disp(getReport(ME, 'extended', 'hyperlinks', 'off')); exit(1); end; exit(0);" > runexamples.log 2>&1
